ce2b11d76e24dbd56b17013313daeb7adee984fd
Andrii <andrii.malaman@edited.de>
2016-06-07 10:43:07 +0200

    Calculate backgroundPosition of zoomed image in mousemove event

--- js-image-zoom.js
+++ js-image-zoom.js
@@ -11,6 +11,7 @@
      *          @param {number} width Image width
      *          @param {number} height Image height
      *          @param {number} zoomWidth  Zoomed image width
+     *          @param {number} scale zoom scale
      */
     return function ImageZoom(container, options) {
         "use strict";
@@ -18,18 +19,43 @@
             return;
         }
         var image = container.children[0];
-        var originalImgWidth = image.getBoundingClientRect().width;
-        var zoomHeight;
+        var originalImgWidth;
+        var originalImgHeight;
         var div = document.createElement('div');
-        var zoomDiv = container.appendChild(div);
+        var zoomDiv;
+        var scaleX;
+        var scaleY;
+        var offset;
+        
         options = options || {};
-
         image.style.width = options.width + 'px' || 'auto';
         image.style.height = options.height + 'px' || 'auto';
+
+        zoomDiv = container.appendChild(div);
         zoomDiv.style.width = options.zoomWidth + 'px';
         zoomDiv.style.height = image.style.height;
         zoomDiv.style.display = 'inline-block';
         zoomDiv.style.backgroundImage = 'url(' + image.src + ')';
+        zoomDiv.style.backgroundRepeat = 'no-repeat';
+
+        function getOffset(el) {
+            if (el) {
+                var elRect = el.getBoundingClientRect();
+                var scrollX = window.scrollX || window.pageXOffset;
+                var scrollY = window.scrollY || window.pageYOffset;
+                return {left: elRect.left + scrollX, top: elRect.top + scrollY};
+            }
+            return {left: 0, top: 0};
+        }
+
+        image.onload=function() {
+            originalImgWidth = image.naturalWidth;
+            originalImgHeight = image.naturalHeight;
+            zoomDiv.style.backgroundSize = originalImgWidth + 'px ' + originalImgHeight + 'px';
+            scaleX = originalImgWidth / options.width;
+            scaleY = originalImgHeight / options.height;
+            offset = getOffset(image)
+        };
 
         var events = {
             handleEvent: function(event) {
@@ -38,11 +64,15 @@
                 }
             },
             handleMouseOver: function(event) {
-
+                var offsetX = event.clientX - offset.left;
+                var offsetY = event.clientY - offset.top;
+                var backgroundTop = offsetX * scaleX;
+                var backgroundRight = offsetY * scaleX;
+                var backgroundPosition = '-' + backgroundTop + 'px ' +  '-' + backgroundRight + 'px';
+                zoomDiv.style.backgroundPosition = backgroundPosition;
             }
         };
 
         image.addEventListener('mousemove', events, false);
-
     }
 }));
